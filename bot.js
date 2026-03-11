cced08dcb68a7bd14e9121402123a1db2fa1f55f
Dandy <dandyraspudi@gmail.com>
2026-03-12 04:23:24 +0700

    init project bot wa

---
+++ bot.js
@@ -0,0 +1,74 @@
+import baileys from "@whiskeysockets/baileys"
+import qrcode from "qrcode-terminal"
+
+const { default: makeWASocket, useMultiFileAuthState } = baileys
+
+async function startBot() {
+
+  const { state, saveCreds } = await useMultiFileAuthState("auth")
+
+  const sock = makeWASocket({
+    auth: state
+  })
+
+  const allowedNumbers = [
+    "6282261977043@s.whatsapp.net",
+    "6288212810834@s.whatsapp.net"
+  ]
+
+  sock.ev.on("creds.update", saveCreds)
+
+  sock.ev.on("connection.update", ({ connection, qr }) => {
+
+    if (qr) {
+      console.log("Scan QR:")
+      qrcode.generate(qr, { small: true })
+    }
+
+    if (connection === "open") {
+      console.log("Bot connected ✅")
+    }
+
+  })
+
+  sock.ev.on("messages.upsert", async ({ messages }) => {
+
+    const msg = messages[0]
+    if (!msg.message) return
+
+    const sender = msg.key.remoteJid
+    if (!allowedNumbers.includes(sender)) return
+
+    const text =
+      msg.message.conversation ||
+      msg.message.extendedTextMessage?.text
+
+    if (text === "halo") {
+
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "Kenapa bub"
+      })
+
+    }
+
+    if (text === "lagi ngapain?" || text === "lagi ngapain") {
+
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "Bentar bub lagi agak full"
+      })
+
+    }
+
+    if (text === "apa ini?" || text === "apa ini bub?" || text === "apa ini kak?") {
+
+      await sock.sendMessage(msg.key.remoteJid, {
+        text: "Ini aku lagi ngetest chat bot 😈"
+      })
+
+    }
+
+  })
+
+}
+
+startBot()
