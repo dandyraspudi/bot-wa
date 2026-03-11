015f80bed2cc3f38b4a35eec3486e146366470e9
Dandy <dandyraspudi@gmail.com>
2026-03-12 05:20:11 +0700

    set allow number

--- bot.js
+++ bot.js
@@ -37,7 +37,7 @@ async function startBot() {
     if (!msg.message) return
 
     const receiver = msg.key.remoteJid
-     if (receiver !== allowedNumbers.includes(receiver)) return
+     if (allowedNumbers.includes(receiver)) return
 
     const text =
       msg.message.conversation ||
